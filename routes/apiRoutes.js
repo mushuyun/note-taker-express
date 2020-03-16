dd7c961c3b6e0ab185405ff41d8673e314df9246
mushuyun <mushuyun@yahoo.com>
2020-03-16 17:24:34 -0400

    upload files

---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+var db = require("../db/db.json");
+const fs = require("fs");
+//const uuid = require(uuid);
+
+module.exports = function(app){
+    app.get("/api/notes", (req, res)=> {
+
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+            if (err) throw err;
+            res.json(data);
+        });
+        
+    });
+       
+        
+    app.post("/api/notes", (req, res) => {
+
+        db.push(req.body);
+        
+        fs.writeFile("./db/db.json", JSON.stringify(db, null, 2), err =>{
+            if (err) throw err;
+            res.json(db);
+            });
+    });   
+       
+
+    app.delete("api/notes/:id", (req, res) => {
+
+         id = req.params.id;
+
+        fs.readFile("./db/db.json", "utf8", (err, data) =>{
+            if (err) throw err;
+
+            data = JSON.parse(data);
+
+            db = data.filter(note => id !== note.id);
+
+            fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) => {
+                if (err) throw err;
+                res.json(db);
+            });
+
+        });
+    });
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+// Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening port: ${PORT}`);
+});
