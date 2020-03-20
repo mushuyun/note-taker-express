d4f42652a5a2839c8bf875969136acf4d2ee055d
mushuyun <mushuyun@yahoo.com>
2020-03-20 17:12:19 -0400

    correcting code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,47 +1,45 @@
 var db = require("../db/db.json");
 const fs = require("fs");
-//const uuid = require(uuid);
+
 
 module.exports = function(app){
-    app.get("/api/notes", (req, res)=> {
-
+      
+    app.get("/api/notes", (req, res) => {
         fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (err) throw err;
-            res.json(data);
+          if (err) throw err;
+          res.json(JSON.parse(data)); // NEED TO PARSE AS JSON
         });
-        
-    });
-       
+      });
         
     app.post("/api/notes", (req, res) => {
 
         let notes = db;
+
         newNote = req.body;
         notes.push(newNote);
         
-        fs.writeFile("./db/db.json", JSON.stringify(db, null, 2), err =>{
+        fs.writeFile("./db/db.json", JSON.stringify(db), err =>{
             if (err) throw err;
             res.json(notes);
             });
     });   
        
-
-    app.delete("api/notes/:id", (req, res) => {
-
-         id = req.params.id;
-
-        fs.readFile("./db/db.json", "utf8", (err, data) =>{
-            if (err) throw err;
-
-            data = JSON.parse(data);
-
-            db = data.filter(note => id !== note.id);
-
-            fs.writeFile("./db/db.json", JSON.stringify(db), (err, data) => {
-                if (err) throw err;
-                res.json(db);
-            });
-
+    app.delete("/api/notes/:id", function (req, res) {
+        
+        let id = req.params.id;
+    
+        fs.readFile("./db/db.json", "utf8", (error, data) => {
+          if (error) throw error;
+    
+          data = JSON.parse(data);
+          db = data.filter(note => id !== note.id);
+    
+          fs.writeFile("./db/db.json", JSON.stringify(db), (error, data) => {
+            if (error) throw error;
+    
+            res.json(db);
+          });
         });
-    });
-}
+      });
+    }
+    
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3009;
 
 const app = express();
 // Sets up the express app to handle data parsing
